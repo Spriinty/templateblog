2252a60e0f9f4ac0cbe70cc69dd559c714ac66fb
Spriinty <quentinpetrequin@gmail.com>
2020-02-20 13:49:19 +0000

    Modif name css/style.css en css/main.css et ajout du fichier style.css dans le dossier templateblog

---
+++ index.php
@@ -0,0 +1,9 @@
+<!-- Dans wp_head ajouter le doctype jusqu'à l'ouverture du body -->
+<?php wp_head(); ?>
+
+
+
+
+
+<!-- Dans wp_footer ajouter le fin de body jusqu'à la fin de HTML -->
+<?php wp_footer() ?> 
