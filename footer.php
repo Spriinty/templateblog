5d6619ae1d74247c1e594e6f6da4ee9ae2aca185
Spriinty <quentinpetrequin@gmail.com>
2020-02-26 09:51:32 +0000

    Ajout responsive footer

--- footer.php
+++ footer.php
@@ -2,17 +2,17 @@
 <footer class="container-fluid bg-footer py-5">
     <div class="container">
         <div class="row align-items-center">
-            <div class="col-4 px-5 pb-5"><img class="logo-footer-size" src="<?php echo get_template_directory_uri(); ?>/assets/logo.png" alt="logo de la promotion des gitBreakers" /></div>
-            <div class="col-4 px-5 pb-5 font-family-flower color-rose">
+            <div class="col-12 col-sm-6 offset-sm-3 col-md-4 offset-md-4 col-lg-4 offset-lg-0 col-xl-4 offset-xl-0 px-5 pb-5 text-center"><img class="logo-footer-size" src="<?php echo get_template_directory_uri(); ?>/assets/logo.png" alt="logo de la promotion des gitBreakers" /></div>
+            <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 px-5 pb-5 font-family-flower text-center text-sm-center text-md-center text-lg-left text-xl-left">
                 <ul class="list-unstyled">
-                    <li><a href="#" class="color-rose">Lorem ipsom dolor</a></li>
-                    <li><a href="#" class="color-rose">Lorem ipsom dolor</a></li>
-                    <li><a href="#" class="color-rose">Lorem ipsom dolor</a></li>
-                    <li><a href="#" class="color-rose">Lorem ipsom dolor</a></li>
-                    <li><a href="#" class="color-rose">Lorem ipsom dolor</a></li>
+                    <li><a href="#" class="color-rose">Accueil</a></li>
+                    <li><a href="#" class="color-rose">Apprenants</a></li>
+                    <li><a href="#" class="color-rose">Articles</a></li>
+                    <li><a href="#" class="color-rose">Projets</a></li>
+                    <li><a href="https://www.accesscodeschool.fr/nous_rejoindre/" class="color-rose">Contact</a></li>
                 </ul>
             </div>
-            <div class="col-4 px-5 pb-5 font-family-flower">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 px-5 pb-5 font-family-flower text-center text-sm-center text-md-center text-lg-left text-xl-left">
             <ul  class="list-unstyled">
                 <li><a href="https://www.accesscodeschool.fr/" class="color-rose">Visitez notre site</a></li>
                     <li><a href="https://www.facebook.com/AccessCodeSchool/?ref=br_rs" class="color-rose">Facebook</a></li>
@@ -23,7 +23,7 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-4 offset-4">
+            <div class="col-12 col-sm-12 col-md-4 offset-md-4 col-lg-4 offset-lg-4 col-xl-4 offset-xl-4">
                 <p class="title-footer m-0 py-3 bordeur-top-footer text-center">©Copyright - Gitbreakers 2020</p>
             </div>
         </div>
