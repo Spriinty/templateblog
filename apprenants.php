e82b709b1201f800c958edfa0fca595abf443071
DesireeBesnard <desireebesnard@gmail.com>
2020-02-27 12:36:11 +0000

    pb flex-wrap a regler dans page apprenants

--- apprenants.php
+++ apprenants.php
@@ -1,15 +1,10 @@
-
 <?php
 /*
 Template Name: Apprenants
 */
 
-// Votre code ici
-
 get_header();
 ?>
-
-<h2 class="text-center">Les apprenants de la promo 33 de l'ACS à Vesoul</h2>
 
 <?php
 
@@ -21,44 +16,52 @@ $args = array(
 
 // 2. on exécute la query
 $my_query = new WP_Query($args);
-echo '<div class="container">
-            <div class="row">';
-// 3. on lance la boucle !
-if ($my_query->have_posts()) : while ($my_query->have_posts()) : $my_query->the_post();
 ?>
+<div class="container-fluid">
+    <div class="container">
+        <h2 class="text-center">Les apprenants de la promo 33 de l'ACS à Vesoul</h2>
 
-        <div class="col-3">
-            <div class="flip-card-apprenants">
-                <div class="flip-card-inner-apprenants">
+        <div class="row d-flex flex-wrap">
+            <?php
+            // 3. on lance la boucle !
+            if ($my_query->have_posts()) : while ($my_query->have_posts()) : $my_query->the_post();
+            ?>
 
-                    <div class="flip-card-front-apprenants text-center">
-                        <div class="d-flex justify-content-center">
-                            <p class="mb-0"><?php the_title(); ?></p>
-                        </div>
-                        <div>
-                            <p><?php the_post_thumbnail(array('class' => "img-fluid")); ?></p>
+                
+                    <div class="col-3 flip-card-apprenants">
+                        <div class="flip-card-inner-apprenants">
+
+                            <div class="flip-card-front-apprenants text-center">
+                                <div>
+                                    <p class="mb-0"><?php the_title(); ?></p>
+                                </div>
+                                <div>
+                                    <p><?php the_post_thumbnail(array('class' => "img-fluid")); ?></p>
+                                </div>
+                            </div>
+                            <div class="flip-card-back-apprenants">
+                                <div class="d-flex justify-content-center">
+                                    <p class="mb-0 mr-2"><a href="<?php echo get_post_meta($post->ID, '_personnes_linkedin', true) ?>"><i class="fa fa-lg fa-linkedin"></i></a></p>
+                                    <p class="mb-0 ml-2">
+                                        <a href="<?php echo get_post_meta($post->ID, '_personnes_github', true) ?>"><img class="img-fluid logo-github" src="<?php echo get_template_directory_uri(); ?>/assets/github.png" /></a>
+                                    </p>
+                                </div>
+                                <div>
+                                    <p><?php the_post_thumbnail(array('class' => "img-fluid")); ?></p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="flip-card-back-apprenants">
-                        <div class="d-flex justify-content-center">
-                            <p class="mb-0 mr-2"><a href="<?php get_post_meta($post->ID, '_personnes_linkedin', true) ?>" target="_blank"><i class="fa fa-lg fa-linkedin"></i></a></p>
-                            <p class="mb-0 ml-2">
-                                <a href="<?php get_post_meta($post->ID, '_personnes_portfolio', true) ?>" target="_blank"><img class="img-fluid logo-github" src="<?php echo get_template_directory_uri(); ?>/assets/github.png" /></a>
-                            </p>
-                        </div>
-                        <div>
-                            <p><?php the_post_thumbnail(array('class' => "img-fluid")); ?></p>
-                        </div>
+                
 
-                    </div>
-                </div>
-            </div>
+            <?php
+                endwhile;
+            endif;
+            // 4. On réinitialise à la requête principale (important)
+            wp_reset_postdata();
+
+            ?>
         </div>
 
-<?php
-    endwhile;
-endif;
-// 4. On réinitialise à la requête principale (important)
-wp_reset_postdata();
-
-?>
+    </div>
+</div>
--- projets-template.php
+++ projets-template.php
@@ -7,17 +7,7 @@
 get_header();
 ?>
 
-<h2>Les apprenants de la promo 33 de l'ACS à Vesoul</h2>
 
-<<<<<<< HEAD
-<?php
-
-// 1. on défini ce que l'on veut
-$args = array(
-  'post_type' => 'projets',
-  'posts_per_page' => -1,
-);
-=======
 <div class="container p-0">
   <div class="main">
     <div class="row mt-5">
@@ -27,36 +17,18 @@ $args = array(
       </div>
       <div class="col-4 mt-3 border-top"></div>
     </div>
->>>>>>> 8622e53b14f45b59c61327a1c798418580108875
 
-// 2. on exécute la query
-$my_query = new WP_Query($args);
+    <?php
 
+    // 1. on défini ce que l'on veut
+    $args = array(
+      'post_type' => 'projets',
+      'posts_per_page' => -1,
+    );
 
+    // 2. on exécute la query
+    $my_query = new WP_Query($args);
 
-<<<<<<< HEAD
-if ($my_query->have_posts()) : while ($my_query->have_posts()) : $my_query->the_post();
-?>
-
-    <div class="col-12">
-      <div class="col-4">
-        <p><?php echo get_post_meta($post->ID, '_projets_description', true); ?></p><br>
-        <p><?php echo get_post_meta($post->ID, '_projets_liens', true); ?></p>
-      </div>
-      <div class="col-8">
-
-      </div>
-    </div>
-
-<?php
-  endwhile;
-endif;
-// 4. On réinitialise à la requête principale (important)
-wp_reset_postdata();
-
-get_footer();
-?>
-=======
     // 3. on lance la boucle !
     if ($my_query->have_posts()) : while ($my_query->have_posts()) : $my_query->the_post();
     ?>
@@ -64,13 +36,11 @@ get_footer();
           <div class="col-4 text-center pt-5 pb-5">
             <h2><?php the_title(); ?></h2>
             <p class="text-break"><?php echo get_post_meta($post->ID, '_projets_description', true); ?></p><br>
-        <p><?php echo get_post_meta($post->ID, '_projets_link', true); ?></p>
-            <!-- <h3><strong>Description:</strong></h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, nisi explicabo nemo velit illo dolorem aut amet ratione similique, dolores, perspiciatis assumenda distinctio fugiat. Ea architecto autem aut incidunt ipsum?</p> -->
+            <p><a href="<?php echo get_post_meta($post->ID, '_projets_link', true); ?>"> Voir le projet</a></p>        
           </div>
 
           <div class="col-8" style='background-image: url("<?php echo wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>"); background-size: cover; background-position-y: center;'></div>
-          </div>
+        </div>
       <?php
       endwhile;
     endif;
@@ -83,5 +53,3 @@ get_footer();
         
   </div>
 </div>
-
->>>>>>> 8622e53b14f45b59c61327a1c798418580108875
