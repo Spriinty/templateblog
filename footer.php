03dddd38ad243aa9730bf9698f9ff4b4edc054a4
chahrazedd14 <shera.mng@gmail.com>
2020-02-28 12:56:49 +0000

    rejout article

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
         <div class="row logo-center">
             <div class="col-12 col-sm-6 offset-sm-3 col-md-4 offset-md-4 col-lg-4 offset-lg-0 col-xl-4 offset-xl-0  text-center px-5 pb-5  "><img class="logo-footer-size" src="<?php echo get_template_directory_uri(); ?>/assets/logoyellow.png" alt="logo de la promotion des gitBreakers" /></div>
             <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 px-5 pb-5 font-family-flower text-center text-sm-center text-md-center text-lg-left text-xl-left">
-                <ul class="list-unstyled">
+                <ul class="list-unstyled raleway">
                     <li><a href="#" class="color-rose raleway">Accueil</a></li>
                     <li><a href="#" class="color-rose raleway">Apprenants</a></li>
                     <li><a href="#" class="color-rose raleway">Articles</a></li>
@@ -14,16 +14,16 @@
             </div>
 
             <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 px-5 pb-5 font-family-flower text-center    text-sm-center text-md-center text-lg-left text-xl-left">
-                <ul class="list-unstyled">
-                    <li><a href="https://www.accesscodeschool.fr/" class="color-rose raleway">Visitez notre site</a></li>
+                <ul class="list-unstyled raleway">
+                    <li><a href="https://www.accesscodeschool.fr/" class="color-rose raleway">OnlineFormaPro</a></li>
                     <li><a href="https://www.facebook.com/AccessCodeSchool/?ref=br_rs" class="color-rose raleway">Facebook</a></li>
-                    <li><a href="https://twitter.com/onlineformapro" class="color-rose raleway">Twitter OnlineFormaPro</a></li>
-                    <li><a href="https://twitter.com/AccessCodeOFP" class="color-rose raleway">Twitter Access Code School</a></li>
-                    <li><a href="#" class="color-rose raleway">Lorem ipsom dolor</a></li>
+                    <li><a href="https://twitter.com/onlineformapro" class="color-rose raleway">Twitter</a></li>
+                    <li><a href="https://twitter.com/AccessCodeOFP" class="color-rose raleway">Twitter</a></li>
+                    <li><a href="https://www.accesscodeschool.fr/blog/" class="color-rose raleway">Blog</a></li>
                 </ul>
             </div>
         </div>
-        <div class="col-6 offset-3">
+        <!-- <div class="col-6 offset-3">
                 <ul class='social pl-0  mb-0 h-100 justify-content-center'>
                     <li class='facebook px-3'>
                         <a>
@@ -46,10 +46,10 @@
                         </a>
                     </li>
                 </ul>
-            </div>
+            </div> -->
         <div class="row">
-            <div class=" col-6 offset-3 text-center py-5">
-                <p class="title-footer m-0 py-3 bordeur-top-footer text-center">©Copyright - Gitbreakers 2020</p>
+            <div class=" col-6 offset-3 text-center py-2 ">
+                <p class="title-footer raleway m-0 bordeur-top-footer py-1 text-center">©Copyright - Gitbreakers 2020</p>
             </div>
         </div>
     </div>
