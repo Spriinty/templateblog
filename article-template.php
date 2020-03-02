89acf52efcf413a6d1e7900811ab6336a016d381
Dev-hrod <rodrigue-cegelly@hotmail.com>
2020-03-02 10:24:42 +0000

    lol commit

--- article-template.php
+++ article-template.php
@@ -37,8 +37,8 @@ get_template_part('banniere', get_post_format());
             ?>
                     <div class="row mb-5 article-height shadow flex-wrap-reverse flex-sm-wrap-reverse flex-md-wrap-reverse">
 
-                        <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 raleway">
-                            <p class="mt-4"><?php the_title(); ?></p>
+                        <div class="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4 raleway text-center">
+                            <h2 class="mt-4"><?php the_title(); ?>...</h2>
                             <p><?php the_excerpt(); ?></p>
                             <div class="btn btn-warning mb-4 mb-sm-4 mb-md-4 mb-lg-0 mb-xl-0"><a class="text-white" href="<?php the_permalink(); ?>">Lire l'article</a></div>
                         </div>
